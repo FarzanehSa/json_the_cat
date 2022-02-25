f019e4074dc7407800871bc5635737987a4c6c6f
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-02-24 22:43:42 -0800

    refactor breedFetcher and add mocha chai test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,37 +1,31 @@
 const request = require('request');
 
+const fetchBreedDescription = function(breadName,callback) {
 
-const breedFetcher = function(url) {
-  
+  const api = 'https://api.thecatapi.com/v1/breeds/search/?q=';
+  const url = api + breadName;
+
   request(url, (error, response, body) => {
     // URL results in an error
     if (error) {
-      console.log(error);
+      callback(error,null);
       return;
     }
     // breed not found
     if (body === '[]') {
-      console.log("\nSadly the requested breed is not found.\n");
+      error = 'Requested breed is not found'
+      callback(error,null);
       return;
     }
-    const data = JSON.parse(body);
-    for (let i = 0; i < data.length; i++) {
-      console.log(`\n ${data[i].description}\n`);
-    }
-  });
-};
 
-// ------------------------------------------
+    const data = JSON.parse(body);
+    callback(null,data[0].description);
+    
+  });
+
+
+};
+  
+module.exports = { fetchBreedDescription };
 
 // const url = 'https://api.thecatapi.com/v1/breeds/search/?q=sib';
-
-const input = process.argv.slice(2);
-if (input.length !== 1) {
-  console.log("\nWe are going to print cat's reed description");
-  console.log('After app name, insert bread name \n');
-  return;
-}
-
-const breed = input[0];
-const url = 'https://api.thecatapi.com/v1/breeds/search/?q=' + breed;
-breedFetcher(url);
